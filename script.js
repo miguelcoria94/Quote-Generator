d9836d47d33d89e9ee3e3b5e4976d2e7ed41076b
miguelcoria94 <miguelcoria94@gmail.com>
2020-10-14 22:43:38 -0500

    done

--- script.js
+++ script.js
@@ -6,25 +6,52 @@ window.addEventListener("DOMContentLoaded", event => {
     const authorText = document.getElementById('author');
     const twitterBtn = document.getElementById('twitter');
     const newQuoteBtn = document.getElementById('new-quote');
+    const loader = document.getElementById('loader');
+    loader.hidden = true;
+    function showLoading() {
+        loader.hidden = false;
+        quoteContainer.hidden = true;
+    }
+
+    function doneLoading() {
+        if (!loader.hidden) {
+            quoteContainer.hidden = false;
+            loader.hidden = true;
+        }
+    }
     
     async function getQuote() {
-        //the free api was down, but I found this one
+        showLoading()
         let largeInt = Math.floor(Math.random() * 80)
         const apiUrl = 'https://type.fit/api/quotes';
         try {
             const response = await fetch(apiUrl);
             const data = await response.json()
-    
+            if (data[largeInt].author === "") {
+                authorText.innerHTML = "Unknown"
+            }
             quoteText.innerHTML = data[largeInt].text
             authorText.innerHTML = data[largeInt].author
-            
+
+            doneLoading();
         } catch (error) {
             console.log('Could not get quote', error);
         }
     }
 
-    //listen for button click
+
+    function tweetQuote() {
+        const quote = quoteText.innerHTML;
+        const author = authorText.innerHTML;
+        const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+        window.open(twitterUrl, '_blank');
+    }
+
     newQuoteBtn.addEventListener('click', event => {
-        getQuote()
+        getQuote();
+    })
+
+    twitterBtn.addEventListener('click', event => {
+        tweetQuote();
     })
 })
